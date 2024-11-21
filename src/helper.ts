76767ce28048b189722bfa7bae7fe98271db1463
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-11-21 01:29:38 -0800

    Create helper function for getting comics

---
+++ src/helper.ts
@@ -0,0 +1,14 @@
+import { getCollection } from 'astro:content';
+
+export async function getComics(order: 'asc' | 'desc' = 'asc') {
+	const comics = await getCollection('comics');
+
+	return comics.sort((a, b) => {
+		const aDateString = a.data.date;
+		const bDateString = b.data.date;
+
+		if (aDateString < bDateString) return order === 'asc' ? 1 : -1;
+		if (aDateString > bDateString) return order === 'asc' ? -1 : 1;
+		else return 0;
+	});
+}
