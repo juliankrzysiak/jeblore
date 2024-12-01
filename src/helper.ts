fcff6e0a8b3e4478d385ba5fe62cd208806a54ef
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-11-30 18:28:11 -0800

    Fix sorting helper function

--- src/helper.ts
+++ src/helper.ts
@@ -7,8 +7,8 @@ export async function getComics(order: 'asc' | 'desc' = 'asc') {
 		const aDateString = a.data.date;
 		const bDateString = b.data.date;
 
-		if (aDateString < bDateString) return order === 'asc' ? 1 : -1;
-		if (aDateString > bDateString) return order === 'asc' ? -1 : 1;
+		if (aDateString < bDateString) return order === 'asc' ? -1 : 1;
+		if (aDateString > bDateString) return order === 'asc' ? 1 : -1;
 		else return 0;
 	});
 }
