2636d0032614d56dd635f41d23397191e04e7186
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-11-30 18:46:00 -0800

    Add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), markdoc(), keystatic()],
-	output: 'hybrid',
+  integrations: [react(), markdoc(), keystatic()],
+  output: 'hybrid',
+  adapter: cloudflare(),
 });
